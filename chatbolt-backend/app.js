65b1751d419ab8088fa5d0cafc610efef7e60f6a
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-10-05 12:40:55 +0300

    Create backend endpoints

--- chatbolt-backend/app.js
+++ chatbolt-backend/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const MongoStore = require('connect-mongo');
 
+const User = require('./models/user');
+
 const app = express();
 
 mongoose.set('strictQuery', false);
@@ -25,5 +27,47 @@ app.use(session({
     client: mongoose.connection.getClient()
   })
 }));
+
+app.get('/api/users', async (req, res) => {
+  if (req.session) {
+    const user = await User.findOne({ sessionId: req.sessionID } );
+    return res.json(user);
+  } else {
+    res.status(404).json({ error: 'No session' });
+  }
+});
+
+app.post('/api/users/create', async (req, res) => {
+  req.session.questionIndex = 0;
+
+  // Date.now() explicitly assigned to a constant to eliminate any 
+  // possible difference in microseconds between the session and user model
+  const currentDate = Date.now();
+  req.session.startedAt = currentDate;
+  try {
+    const user = new User ({
+      sessionId: req.sessionID,
+      sessionStart: currentDate
+    });
+    const newUser = await user.save();
+    return res.status(201).json(newUser);
+  } catch (err) {
+    return res.status(400).json({ err });
+  }
+});
+
+app.put('/api/users/:id', async (req, res) => {
+  try{
+    const updatedAnswers = await User.findOneAndUpdate(
+      { sessionId: req.params.id },
+      { $set: { answers: req.body.answers } },
+      { new: true }
+    );
+    // req.session.questionIndex = TODO
+    return res.status(200).json(updatedAnswers)
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
 
 module.exports = app;
