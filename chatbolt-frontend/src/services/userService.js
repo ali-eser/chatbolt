268c72accfccf4c47d3c661f48a78cc584181ece
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-10-05 23:43:43 +0300

    Allow PUT requests from frontend

--- chatbolt-frontend/src/services/userService.js
+++ chatbolt-frontend/src/services/userService.js
@@ -1,16 +1,26 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3001'
+
+// allow browser to hand back cookies with every request
 axios.defaults.withCredentials = true
 const baseURL = '/api/users'
 
+// fetch existing user info
 const getUserData = async () => {
   const res = await axios.get(baseURL)
   return res.data
 };
 
+// create a new user
 const createNewUser = async () => {
   const res = await axios.post(`${baseURL}/create`)
   return res.data
 }
 
-export default { getUserData, createNewUser }
+// update user answers on database
+const updateUserData = async (answers, sessionId) => {
+  const res = await axios.put(`${baseURL}/${sessionId}`, answers)
+  return res.data
+}
+
+export default { getUserData, createNewUser, updateUserData }
